bd1668d2eb0ff3c7586be5e3cf8f8fcdea1d2ba7
Camilo318 <camiloa0318@gmail.com>
2020-08-31 15:26:59 -0500

    Add colors for each pokemon BackGroundColor Changed z-index: 1 for the Header

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -1,11 +1,31 @@
 import React from 'react'
 
 const Pokemon = (props) => {
-    const {name, height, weight} = props
+    const {name, height, weight, types} = props
     const nameUpper = name[0].toUpperCase() + name.slice(1)
+    const colors = {
+        fire: '#FDDFDF',
+        grass: '#DEFDE0',
+        electric: '#FCF7DE',
+        water: '#DEF3FD',
+        ground: '#f4e7da',
+        rock: '#d5d5d4',
+        fairy: '#fceaff',
+        poison: '#98d7a5',
+        bug: '#f8d5a3',
+        dragon: '#97b3e6',
+        psychic: '#eaeda1',
+        flying: '#F5F5F5',
+        fighting: '#E6E0D4',
+        normal: '#F5F5F5'
+    }
+
+    const color = types[0].type.name
+
     return (
         <div className='pokemon'>
-            <div className="pokemon__img">
+            <div className="pokemon__img"
+            style={{backgroundColor: colors[color] }}>
                 <img src={`https://pokeres.bastionbot.org/images/pokemon/${props.id}.png`} alt={name}/>
             </div>
             <h4>{nameUpper}</h4>
