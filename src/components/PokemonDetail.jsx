92c4344d8bc06abb9023293ae1e75deb7c4067ea
Camilo318 <camiloa0318@gmail.com>
2020-09-01 09:55:55 -0500

    Add info to the PokemonDetail component

--- src/components/PokemonDetail.jsx
+++ src/components/PokemonDetail.jsx
@@ -3,17 +3,25 @@ import React, { useState, useEffect } from 'react'
 const PokemonDetail = ({match}) => {
     const { id } = match.params
     const [info, setInfo] = useState({})
+
     useEffect(() => {
         const getPokemonInfo = async () => {
             const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
             const data = await res.json()
             console.log(data)
             setInfo(data)
+
         }
         getPokemonInfo()
     }, [])
     
-    
+    const { name, height, weight} = info
+    const capitalize = (x) => {
+        return x && x[0].toUpperCase() + x.slice(1)
+    }
+    const getTypes = (x) => {
+        return x && x.map(type => `#${type.type.name}`).join(' ')
+    }
     return (
         <div className='pokemon-detail'>
             <div className="pokemon-detail__image">
@@ -21,8 +29,19 @@ const PokemonDetail = ({match}) => {
             </div>
 
             <div className="pokemon-detail__info">
-                <h3>{info.name}</h3>
-
+                <h3>{capitalize(name)} {id.padStart(3,0)}</h3>
+                <p>Height: {height/10}m</p>
+                <p>Weight: {weight/10}kg</p>
+                <p>Base Experience: {info.base_experience}</p>
+                {info.abilities &&
+                <p>Abilities: {info.abilities.map((a,i) => {
+                    return (
+                        <span key={i}>
+                            {capitalize(a.ability.name)}
+                        </span>
+                    )
+                })}</p>}
+                <p>Types: {getTypes(info.types)}</p>
             </div>
         </div>
     )
