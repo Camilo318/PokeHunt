5bf01e7e09bc1ef597f0c14be9da436d47c22330
Camilo318 <camiloa0318@gmail.com>
2020-09-06 09:17:02 -0500

    Add Footer component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import Home from './Home'
 import Header from './Header'
 import { HashRouter, Switch, Route } from 'react-router-dom'
 import PokemonDetail from './PokemonDetail'
+import Footer from './Footer'
 
 const App = () => {
     return (
@@ -12,6 +13,7 @@ const App = () => {
                 <Route exact path='/' component={Home} />
                 <Route exact path='/pokemon/:id' component={PokemonDetail} />
             </Switch>
+            <Footer/>
         </HashRouter>
         
     )
---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className="footer__container">
+                <p>
+                    Made with 💜 by <a href="https://github.com/Camilo318/PokeHunt" target='__blank'>
+                        Camilo Perilla
+                    </a>
+                </p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
