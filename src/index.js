ecf42d5d783d7af2c52ad1acff0eecaeab5786d6
Camilo318 <camiloa0318@gmail.com>
2020-11-18 13:24:34 -0500

    Remove json file for initial state

--- src/index.js
+++ src/index.js
@@ -5,8 +5,12 @@ import App from './components/App'
 import './assets/styles/style.scss'
 import { Provider } from 'react-redux'
 import { createStore, compose } from 'redux'
-import initialState from './myPokemons.json'
 import reducer from './reducers/index.js'
+
+const initialState = {
+    myPokemons: [],
+    showAside: false
+}
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
 || compose
