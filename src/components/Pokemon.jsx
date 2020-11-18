5c88bc547208a87764016edc8e158a95cf047318
Camilo318 <camiloa0318@gmail.com>
2020-11-18 17:29:17 -0500

    Add loader for each pokemon image Aside style tweaked

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -1,24 +1,27 @@
-import React from 'react'
+import React, { useState }from 'react'
 import { Link } from 'react-router-dom'
 import Colors from '../colors.json'
+import Loader from '../assets/images/pokemon.svg'
+
 
 const Pokemon = (props) => {
     const {name, types} = props
     const nameUpper = name[0].toUpperCase() + name.slice(1)
     
     const color = types[0].type.name
-
+    const [didLoad, setDidLoad] = useState(false)
     return (
         <div className='pokemon'>
             <div className="pokemon__img"
             style={{backgroundColor: Colors[color] }}>
+                <img src={Loader} alt="loader" className='loader'
+                style={{display: didLoad ? 'none' : 'block'}}/> 
                 <Link to={`/pokemon/${props.id}`}>
                     <img
                     src={`https://pokeres.bastionbot.org/images/pokemon/${props.id}.png`}
                     alt={name}
-                    loading="lazy"
-                    onLoad={() => console.log('Loading Image')}
-                    
+                    style={{display: didLoad ? 'block' : 'none'}}
+                    onLoad={() => setDidLoad(true)}
                     />
                 </Link>
             </div>
